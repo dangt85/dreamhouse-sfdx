1bbe8f4d4677b27f210aaa887e639a7a811add38
Daniel Gonzalez <dangt85@gmail.com>
2017-08-30 11:03:44 -0400

    fix default caculated down payment value

--- force-app/main/default/aura/MortgageCalculator/MortgageCalculatorController.js
+++ force-app/main/default/aura/MortgageCalculator/MortgageCalculatorController.js
@@ -3,6 +3,9 @@
     	helper.calculateMonthlyPayment(component, event);
 	},
     recordUpdated : function(component) {
-        component.set("v.principal", component.get("v.property").Price__c);
+        var principal = component.get("v.property").Price__c;
+        var downPayment = principal * 0.05;
+        component.set("v.principal", principal);
+        component.set("v.downPayment", downPayment);
 	}
 })

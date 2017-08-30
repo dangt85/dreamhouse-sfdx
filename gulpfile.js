c083d5df9a3d8916b77cc30fdb79a3ccf1a2b503
Daniel Gonzalez <dangt85@gmail.com>
2017-08-30 09:48:29 -0400

    adds gulp watch to push to dev org automatically

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+const zip = require('gulp-zip')
+const shell = require('gulp-shell')
+
+gulp.task('push', shell.task('sfdx force:source:push'))
+
+gulp.task('build', () => {
+    console.log('build called')
+    return gulp.src('static-resources-src/app/*')
+        .pipe(zip('app.resource'))
+        .pipe(gulp.dest('force-app/main/default/staticresources/'))
+})
+
+gulp.task('watch', () => {
+    gulp.watch(`static-resources-src/app/**/*.*`, ['build'])
+    gulp.watch('force-app/main/default/**/*', ['push'])
+})
